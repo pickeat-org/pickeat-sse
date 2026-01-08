7fd873e503f8f27d7a4e535d8cc2a7af8cb06fa9
supernovaMK <minkikim2002@gmail.com>
2026-01-08 13:38:17 +0900

    Initial commit

---
+++ src/main/java/io/kr/pickeat/pickeatsse/PickeatSseApplication.java
@@ -0,0 +1,13 @@
+package io.kr.pickeat.pickeatsse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickeatSseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickeatSseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/kr/pickeat/pickeatsse/PickeatSseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kr.pickeat.pickeatsse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickeatSseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
